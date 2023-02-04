c1f9ba3e3e6e819e6e29b51ca81490b9baa551e2
Jesse Gronsbell <79216661+jlgrons@users.noreply.github.com>
2023-02-04 08:20:36 -0500

    Update example.R

--- example/example.R
+++ example/example.R
@@ -1,9 +1,16 @@
 
 devtools::install_github(repo = "https://github.com/ChuanHong/ssROCtesting", force=T)
 library(ssROC)
-n <- 300; N <- 5000 - 300; p <- 0.3; boot <- FALSE; nbt <- 2; setting <- 1; seed <- 1
-m2 <- 0.3; m1 <- 0.7; s2 <- 0.2; s1 <- 0.3
+n <- 300
+N <- 5000 - 300
+p <- 0.3
+boot <- FALSE 
+nbt <- 2 
+m2 <- 0.3
+m1 <- 0.7
+s2 <- 0.2; s1 <- 0.3
 my_data <- data_generation(n, N, m1, s1, m2, s2, p, misspec=F)
+
 Y <- my_data[, 'Y_miss']
 S <- my_data[, 'S']
 labeled_ind <- which(!is.na(Y))
@@ -16,6 +23,6 @@ roc.sl <- supervised(St, Yt)
 roc.ssROC <- ssROC(S, Y)
 
 ## pertubation
-roc.sl.pert=pertubation(nbt=20, S_labeled=St,Y_labeled=Yt, S=S, Y=Y, method="supervised")
-roc.ssROC.pert=pertubation(nbt=20, S_labeled=St,Y_labeled=Yt, S=S, Y=Y, method="ssROC")
+roc.sl.pert=pertubation(nbt=nbt, S_labeled=St,Y_labeled=Yt, S=S, Y=Y, method="supervised")
+roc.ssROC.pert=pertubation(nbt=nbt, S_labeled=St,Y_labeled=Yt, S=S, Y=Y, method="ssROC")
 
