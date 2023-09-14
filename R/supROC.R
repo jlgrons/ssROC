b2c876ae9d034fc0570d50f50f7da0d3f5a25637
Jianhui Gao <58570421+jianhuig@users.noreply.github.com>
2023-09-14 10:08:28 -0400

    add transform_score

--- R/supROC.R
+++ R/supROC.R
@@ -8,15 +8,22 @@
 #' @param Y_labeled outcome Y in labeled set
 #' @param W_labeled optional vector of weights for labeled set
 #' @param fpr_vals desired fpr sequence for output
+#' @param transform_score Whether to use ecdf to transform the score
 #' @return matrix containing supROC estimates at fpr_vals
 #' @export
 
 supROC <- function(S_labeled,
                    Y_labeled,
                    W_labeled = NULL,
-                   fpr_vals = seq(0.01, 0.99, by = 0.01)) {
+                   fpr_vals = seq(0.01, 0.99, by = 0.01),
+                   transform_score = TRUE) {
   if (is.null(W_labeled)) {
     W_labeled <- rep(1, length(S_labeled))
+  }
+
+  if(transform_score){
+    S_ecdf <- stats::ecdf(S_labeled)
+    S_labeled <- S_ecdf(S_labeled)
   }
 
   result <- roc(
